4a353454bd70ab9d671a4101d3977e151ea27f17
Kyle Netterville <kylenetterville@Kyles-Air-2.attlocal.net>
2024-08-04 14:29:22 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('gitButton').addEventListener('click', function() {
+  window.location.href = "https://github.com/dkdkdkd222";
+})
+
+document.getElementById('frontendButton').addEventListener('click', function() {
+  window.location.href = "https://www.frontendmentor.io/profile/dkdkdkd222";
+})
+
+document.getElementById('linkedInButton').addEventListener('click', function() {
+  window.location.href = "https://www.linkedin.com/in/kyle-netterville-171246277/";
+})
+
+document.getElementById('twitterButton').addEventListener('click', function() {
+  window.location.href = "https://x.com/KyleNetterville";
+})
+
+
+document.getElementById('instagramButton').addEventListener('click', function() {
+  window.location.href = "https://instagram.com/";
+})
